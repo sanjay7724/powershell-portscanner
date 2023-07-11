1ebae62e1c8cbd7508b536f214acadac5a35fb16
sanjay7724 <sanjaydwells@gmail.com>
2023-07-11 11:15:51 +0530

    port scanner

---
+++ portscanner.ps1
@@ -0,0 +1,12 @@
+﻿$range = 130..140
+$count = 0
+foreach($port in $range){
+    $result = tnc localhost -port $port
+    if(echo $result | select | where-object -propery PingSucceeded -eq true){
+        $count = $count+1
+    }else{
+        continue
+    }
+}
+$count = $count+1
+echo $count
